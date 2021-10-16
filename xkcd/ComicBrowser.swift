44affcafff45bbb29fb362f2eecc5394899aae2a
emin <emin.roblack@gmail.com>
2021-10-16 20:33:07 +0200

    add mock data and test view

--- xkcd/ComicBrowser.swift
+++ xkcd/ComicBrowser.swift
@@ -2,12 +2,26 @@ import SwiftUI
 
 struct ComicBrowser: View {
     
+    let testComic = Comic(num: 614,
+                          title: "Woodpecker",
+                          alt: "If you don't have an extension cord I can get that too.  Because we're friends!  Right?",
+                          img: "https://imgs.xkcd.com/comics/woodpecker.png")
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20, style: .continuous)
-                .fill(Color.purple)
-            Text("ComicBrowser")
-                .foregroundColor(Color.white)
+            AsyncImage(url: testComic.imgUrl) { image in
+                image
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding()
+            } placeholder: {
+                Color.red
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.gray)
+            )
+            .padding()
         }
     }
 }
